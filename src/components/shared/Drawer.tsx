155969fb2d012351dea1ce35d17e5f34f1efd7f8
Fathi <babayejuadesina@gmail.com>
2024-04-25 01:03:54 +0100

    feat: mobile nav drawer

---
+++ src/components/shared/Drawer.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import * as React from "react";
+// import { BsMenuButtonWideFill } from "@/components/ui/BsMenuButtonWideFill"
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { HiOutlineMenu } from "react-icons/hi";
+import Link from "next/link";
+import { twMerge } from "tailwind-merge";
+import { usePathname } from "next/navigation";
+import { IoCloseOutline } from "react-icons/io5";
+
+interface INav {
+  name: string;
+  href: string;
+}
+
+export function NavDrawer() {
+  const NAV_LINKS: INav[] = [
+    {
+      name: "Home",
+      href: "/",
+    },
+    {
+      name: "About us",
+      href: "/about",
+    },
+    {
+      name: "Approach",
+      href: "/approach",
+    },
+    {
+      name: "Portfolio",
+      href: "",
+    },
+    {
+      name: "Community",
+      href: "/community",
+    },
+  ];
+
+  const pathname = usePathname();
+  const [open, setOpen] = React.useState<boolean>(false);
+
+  return (
+    <Drawer direction="right" open={open} onOpenChange={setOpen}>
+      {!open ? (
+        <DrawerTrigger asChild>
+          <button className="text-3xl">
+            <HiOutlineMenu />
+          </button>
+        </DrawerTrigger>
+      ) : (
+        <DrawerClose>
+          <button className="text-3xl text-black-8">
+            <IoCloseOutline />
+          </button>
+        </DrawerClose>
+      )}
+      <DrawerContent className="top-0 right-0 ml-auto mt-16 h-fit rounded-none">
+        <nav className="flex flex-col gap-6 px-5 pb-8">
+          {NAV_LINKS.map((link) => (
+            <Link
+              href={link.href}
+              key={link.name}
+              role="menuitem"
+              className={twMerge(
+                "uppercase text-sm font-semibold hover:text-primary transition duration-300",
+                pathname === link.href && "text-primary"
+              )}
+            >
+              {link.name}
+            </Link>
+          ))}
+        </nav>
+      </DrawerContent>
+    </Drawer>
+  );
+}
--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -6,6 +6,7 @@ import React from "react";
 import { twMerge } from "tailwind-merge";
 import { usePathname } from "next/navigation";
 import LinkButton from "./Button";
+import { NavDrawer } from "./Drawer";
 
 interface INav {
   name: string;
@@ -39,7 +40,7 @@ const Header = () => {
   const pathname = usePathname();
 
   return (
-    <header className="h-16 md:h-20 px-5 sm:px-10 xl:px-28 flex items-center border-b border-b-[#EAECED]">
+    <header className="h-16 md:h-20 px-5 sm:px-10 xl:px-28 flex items-center border-b border-b-[#EAECED] bg-white z-[100]">
       <div className="flex gap-6 justify-between items-center w-full self-stretch">
         <div className="w-32 md:w-40">
           <Image
@@ -74,8 +75,11 @@ const Header = () => {
             buttonText="Investor Login"
             href="/investors/login"
             showIcon={true}
-            className="border border-primary text-primary text-sm h-11 hover:bg-primary hover:text-white transition duration-300"
+            className="border border-primary text-primary text-sm h-11 hover:bg-primary hover:text-white transition duration-300 hidden lg:flex"
           />
+          <div className="lg:hidden">
+            <NavDrawer />
+          </div>
         </div>
       </div>
     </header>
